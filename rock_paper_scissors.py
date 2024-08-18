64a4be4315c1be5a00b32934776975f51fc91963
Kalin Yanev <klyanevv@gmail.com>
2024-08-18 17:00:52 +0300

    Second commit added more features.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,42 +1,51 @@
 import random
-
+import math
 choices = ["rock", "paper", "scissors"]
 
 player = None
 player_score = 0
 computer_score = 0
+games_to_play=int(input("Enter how many games you wish to play: "))
+games_played=0
+games_needed_to_win=math.ceil(games_to_play/2)
 
-while player_score != 3 or computer_score != 3:
+while player_score != games_needed_to_win or computer_score != games_needed_to_win:
     computer = random.choice(choices)
     print("Player score is: ", player_score)
     print("Computer score is: ", computer_score)
-    if player_score == 3:
+    if player_score == games_needed_to_win:
         print("PLAYER WINS!!!")
         break
-    elif computer_score == 3:
+    elif computer_score == games_needed_to_win:
         print("COMPUTER WINS...")
         break
 
     player = input("rock, paper, scissors: ").lower()
-    if player != "rock" or "paper" or "scissors":
+    if player not in ["rock", "paper", "scissors"]:
         print("Enter valid choice!!!!")
     if player == computer:
         print("Computer: ", computer)
         print("Player: ", player)
         print("Tie!")
+        games_played+=1
+        print("Games played: ",games_played)
+        
 
     elif player == "rock":
         if computer == "paper":
             print("Computer: ", computer)
             print("Player: ", player)
             print("Computer wins")
-
+            games_played+=1
+            print("Games played: ",games_played)
             player_score += 0
             computer_score += 1
         if computer == "scissors":
             print("Computer: ", computer)
             print("Player: ", player)
             print("Player wins")
+            games_played+=1
+            print("Games played: ",games_played)
 
             player_score += 1
             computer_score += 0
@@ -46,6 +55,8 @@ while player_score != 3 or computer_score != 3:
             print("Computer: ", computer)
             print("Player: ", player)
             print("Player wins")
+            games_played+=1
+            print("Games played: ",games_played)
 
             player_score += 1
             computer_score += 0
@@ -53,6 +64,8 @@ while player_score != 3 or computer_score != 3:
             print("Computer: ", computer)
             print("Player: ", player)
             print("Computer wins")
+            games_played+=1
+            print("Games played: ",games_played)
 
             player_score += 0
             computer_score += 1
@@ -62,6 +75,8 @@ while player_score != 3 or computer_score != 3:
             print("Computer: ", computer)
             print("Player: ", player)
             print("Player wins")
+            games_played+=1
+            print("Games played: ",games_played)
 
             player_score += 1
             computer_score += 0
@@ -69,6 +84,8 @@ while player_score != 3 or computer_score != 3:
             print("Computer: ", computer)
             print("Player: ", player)
             print("Computer wins")
+            games_played+=1
+            print("Games played: ",games_played)
 
             player_score += 0
             computer_score += 1
